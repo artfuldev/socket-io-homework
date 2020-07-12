62c2c9dfbfe926531e0b939314487776b3b92c3b
Sudarsan Balaji <Sudarsan.Balaji@artfuldev.com>
2020-07-12 07:48:41 +0530

    Add nickname-specific events

--- events.js
+++ events.js
@@ -3,5 +3,7 @@
   exports.CHAT_MESSAGE_RECEIVED = "chat:message_received"
   exports.USER_CONNECTED = "user:connected"
   exports.USER_DISCONNECTED = "user:disconnected"
+  exports.NICKNAME_REQUESTED = "nickname:requested"
+  exports.NICKNAME_OBTAINED = "nickname:obtained"
 }(typeof exports === 'undefined' ? this.events = {} : exports));
 
