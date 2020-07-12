342e7640711e083d8056f841378d5c8f939ca187
Sudarsan Balaji <Sudarsan.Balaji@artfuldev.com>
2020-07-12 09:09:24 +0530

    Show previously connected users in active users list

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var events = require('./events');
 
-const nicknames = new Set();
+const active_nicknames = new Set();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/src/index.html');
@@ -29,8 +29,8 @@ const {
 } = events
 
 const nickname = (requested_nickname, suffix = "") => {
-  if (!nicknames.has(requested_nickname + suffix)) {
-    nicknames.add(requested_nickname + suffix);
+  if (!active_nicknames.has(requested_nickname + suffix)) {
+    active_nicknames.add(requested_nickname + suffix);
     return requested_nickname + suffix;
   }
   return nickname(requested_nickname, "#" + Math.floor(1000 + Math.random() * 9000));
@@ -40,9 +40,10 @@ io.on('connection', socket => {
   socket.on(NICKNAME_REQUESTED, requested_nickname => {
     const user = nickname(requested_nickname);
     socket.emit(NICKNAME_OBTAINED, user);
+    active_nicknames.forEach(nickname => { if (user !== nickname) socket.emit(USER_CONNECTED, nickname); });
     socket.broadcast.emit(USER_CONNECTED, user);
     socket.on(CHAT_MESSAGE_SENT, message => socket.broadcast.emit(CHAT_MESSAGE_RECEIVED, { from: user, message }));
-    socket.on('disconnect', () => { io.emit(USER_DISCONNECTED, user); nicknames.delete(user); });
+    socket.on('disconnect', () => { io.emit(USER_DISCONNECTED, user); active_nicknames.delete(user); });
     socket.on(TYPING_STARTED, () => socket.broadcast.emit(TYPING_STARTED, user));
     socket.on(TYPING_STOPPED, () => socket.broadcast.emit(TYPING_STOPPED, user));
   });
