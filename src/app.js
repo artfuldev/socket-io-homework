804ec806f4c4ce2af8a87f09593aaa0da3636d7b
Sudarsan Balaji <Sudarsan.Balaji@artfuldev.com>
2020-07-12 06:52:15 +0530

    Make cleaner

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const { CHAT_MESSAGE_SENT, CHAT_MESSAGE_RECEIVED } = events
 
 $(function () {
-  var socket = io();
-  $('form').submit(function(e){
-    e.preventDefault(); // prevents page reloading
-    socket.emit(CHAT_MESSAGE_SENT, $('#m').val());
-    $('#m').val('');
-    return false;
+
+  const socket = io();
+  const $input = $('#message-input');
+  const $messages = $('#messages');
+  const $form = $('#message-input-form');
+
+  $form.submit(event => {
+    event.preventDefault();
+    event.stopPropagation();
+    socket.emit(CHAT_MESSAGE_SENT, $input.val())
+    $input.val('');
   });
-  socket.on(CHAT_MESSAGE_RECEIVED, function(msg){
-    $('#messages').append($('<li>').text(msg));
+
+  socket.on(CHAT_MESSAGE_RECEIVED, message => {
+    $messages.append($('<li>').text(message));
   });
+
 });
